cdbe4ef9503a3b6a9b7555a75dbf97e76f47018c
Santoshd561 <Santoshd561@gmail.com>
2019-06-05 21:35:53 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import TodoList from './TodoList'
+import Todo from './todo'
 
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<TodoList />, document.getElementById('root'));
+ReactDOM.render(<Todo />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
